c30b6d9d0e70e59d6124fd546dd5d55676050270
Connor Goddard <connorlukegoddard@gmail.com>
2015-02-22 17:25:12 +0000

    Split image-based and feature-based tracking inot seperate projects and updated Travis CI config file accordingly.

---
+++ experiments/optical_flow_lk_image_template_match/optical_flow_lk_image_template_match/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  optical_flow_lk_image_template_match
+//
+//  Created by Connor Goddard on 22/02/2015.
+//  Copyright (c) 2015 Connor Goddard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
