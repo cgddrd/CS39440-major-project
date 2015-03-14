f21f358e592217f25b7b6063cb2e1ada63658827
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-14 16:41:05 +0000

    Initial commit of python Travis-CI test.

---
+++ experiments/template_matching_scaling/template_matching_scaling/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  template_matching_scaling
+//
+//  Created by Connor Goddard on 13/03/2015.
+//  Copyright (c) 2015 Connor Goddard. All rights reserved.
+//
+
+#include <iostream>
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
