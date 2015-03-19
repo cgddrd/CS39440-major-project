ea7a7a07f0e19fe62d47fd6c0f30f41b2c999ddc
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-19 11:02:24 +0000

    Completed GUI switch from OpenCV tools to 'matplotlib' Python library. Closes #62, #63 and #64.

--- experiments/geometric_scaling_python/image_template.py
+++ experiments/geometric_scaling_python/image_template.py
@@ -3,21 +3,22 @@ from __future__ import division
 __author__ = 'connorgoddard'
 
 import math
-
 import cv2
 import argparse
 import matplotlib.pyplot as plt
 
 line_coords = []
-
-count = 0
+point_count = 0
 slope = 0
+lock_gui = False
+new_img = 0
+original_img = 0
 
 
 def calc_point(start_point, end_point, start_point2, end_point2):
     global slope
 
-    height, width, depth = img1.shape
+    height, width, depth = new_img.shape
 
     i = start_point[1]
 
@@ -39,11 +40,11 @@ def calc_point(start_point, end_point, start_point2, end_point2):
             new_x = start_point[0]
             new_x2 = start_point2[0]
 
-        cv2.circle(img1, (int(new_x), new_y), 2, (0, 0, 255), -1)
+        cv2.circle(new_img, (int(new_x), new_y), 2, (0, 0, 255), -1)
 
-        cv2.circle(img1, (int(new_x2), new_y), 2, (0, 0, 255), -1)
+        cv2.circle(new_img, (int(new_x2), new_y), 2, (0, 0, 255), -1)
 
-        cv2.line(img1, (int(new_x), new_y), (int(new_x2), new_y), (0, 255, 0), 1)
+        cv2.line(new_img, (int(new_x), new_y), (int(new_x2), new_y), (255, 255, 0), 1)
 
         i += 1
 
@@ -51,7 +52,7 @@ def calc_point(start_point, end_point, start_point2, end_point2):
 def calc_point_reflection(start_point, end_point):
     global slope
 
-    height, width, depth = img1.shape
+    height, width, depth = new_img.shape
 
     half_width = width / 2
 
@@ -78,11 +79,11 @@ def calc_point_reflection(start_point, end_point):
             new_x = start_point[0]
             new_x2 = start_point2[0]
 
-        cv2.circle(img1, (int(new_x), new_y), 2, (0, 0, 255), -1)
+        cv2.circle(new_img, (int(new_x), new_y), 2, (0, 0, 255), -1)
 
-        cv2.circle(img1, (int(new_x2), new_y), 2, (0, 0, 255), -1)
+        cv2.circle(new_img, (int(new_x2), new_y), 2, (0, 0, 255), -1)
 
-        cv2.line(img1, (int(new_x), new_y), (int(new_x2), new_y), (0, 255, 0), 1)
+        cv2.line(new_img, (int(new_x), new_y), (int(new_x2), new_y), (0, 255, 255), 1)
 
         i += 1
 
@@ -98,50 +99,98 @@ def add_new_point(mouse_x, mouse_y):
     line_coords.append((mouse_x, mouse_y))
 
 
-def handle_mouse(event, x, y, flags, other):
-    global count
+def on_mouse_click(event):
+    global new_img, point_count
 
-    if event == cv2.EVENT_LBUTTONDOWN:
+    toolbar = plt.get_current_fig_manager().toolbar
 
-        if count < 4:
+    print toolbar.mode
+
+    if toolbar.mode == '' and (event.xdata != None and event.ydata != None):
+
+        x = int(event.xdata)
+        y = int(event.ydata)
+
+        if point_count < 4 and (lock_gui is False):
 
             add_new_point(x, y)
-            cv2.circle(img1, (x, y), 2, (255, 0, 0), -1)
+            cv2.circle(new_img, (x, y), 2, (255, 0, 0), -1)
 
             # If we have an even number of points, then we can draw a line between the last two points added to the collection.
             if len(line_coords) % 2 == 0:
-                cv2.line(img1, (line_coords[len(line_coords) - 1][0], line_coords[len(line_coords) - 1][1]),
+                cv2.line(new_img, (line_coords[len(line_coords) - 1][0], line_coords[len(line_coords) - 1][1]),
                          (line_coords[len(line_coords) - 2][0], line_coords[len(line_coords) - 2][1]), (0, 255, 0), 1)
 
-            count += 1
+            update_image_gui(new_img)
+            point_count += 1
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("inputImage", help="the first image")
-args = parser.parse_args()
+def on_key_press(event):
+    global lock_gui
 
-img1 = cv2.imread(args.inputImage, cv2.IMREAD_COLOR)
+    if event.key == 'r':
+        reset_image_gui()
+
+    elif event.key == 'c':
+
+        if lock_gui is False and point_count == 4:
+            calc_point(line_coords[0], line_coords[1], line_coords[2], line_coords[3])
+            update_image_gui(new_img)
+            lock_gui = True
+
+    elif event.key == 'v':
+
+        if lock_gui is False and point_count == 2:
+            calc_point_reflection(line_coords[0], line_coords[1])
+            update_image_gui(new_img)
+            lock_gui = True
+
+    elif event.key == 'q':
+        plt.close()
 
 
-def onclick(event):
-    if event.xdata != None and event.ydata != None:
-        print(int(event.xdata), int(event.ydata))
+def setup_image_gui(image):
+    plt.axis("off")
+
+    canvas = plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    canvas.figure.canvas.mpl_connect('button_press_event', on_mouse_click)
+    canvas.figure.canvas.mpl_connect('key_press_event', on_key_press)
+
+    plt.show()
 
 
-def onpress(event):
-    if event.key == 'q':
-        img2 = cv2.imread("../eval_data/motion_images/flat_10cm/IMG12.JPG", cv2.IMREAD_COLOR)
-        plt.clf()
-        plt.axis("off")
-        plt.imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
-        plt.draw()
-        # plt.show()
+def update_image_gui(image):
+    plt.clf()
+    plt.axis("off")
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.draw()
 
 
-plt.axis("off")
+def reset_image_gui():
+    global new_img, point_count, lock_gui
 
-implot = plt.imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
-cis = implot.figure.canvas.mpl_connect('button_press_event', onclick)
-cikey = implot.figure.canvas.mpl_connect('key_press_event', onpress)
+    new_img = original_img.copy()
+    update_image_gui(new_img)
 
-plt.show()
+    point_count = 0
+    line_coords[:] = []
+    lock_gui = False
+
+
+def main():
+    global original_img, new_img
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputImage", help="the first image")
+    args = parser.parse_args()
+
+    original_img = cv2.imread(args.inputImage, cv2.IMREAD_COLOR)
+
+    new_img = original_img.copy()
+
+    setup_image_gui(new_img)
+
+
+if __name__ == '__main__':  # if the function is the main function ...
+    main()
