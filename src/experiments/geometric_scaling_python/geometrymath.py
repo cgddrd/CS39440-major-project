6e18df2fc4917fdf146b4c9407626ff9b77a5120
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-19 17:51:48 +0000

    Added new Python classes and generally tidied up to conform to Python code standards.

---
+++ src/experiments/geometric_scaling_python/geometrymath.py
@@ -0,0 +1,82 @@
+from __future__ import division
+import math
+
+__author__ = 'connorgoddard'
+
+
+def calc_vec_magnitude(point_1, point_2):
+    vx = (point_2[0] - point_1[0])
+    vy = (point_2[1] - point_1[1])
+
+    return math.sqrt((vx * vx) + (vy * vy))
+
+
+def calc_line_points_reflection(start_point, end_point, image_height, image_width):
+    half_width = image_width / 2
+
+    start_point2 = (half_width + (half_width - start_point[0]), start_point[1])
+    end_point2 = (half_width + (half_width - end_point[0]), end_point[1])
+
+    i = start_point[1]
+
+    coords1 = []
+    coords2 = []
+
+    while i < image_height:
+
+        new_y = i
+
+        # If we happen to have a perfectly vertical line, then we have no slope (prevents division by 0 error)
+        if (end_point[0] - start_point[0]) != 0:
+
+            slope = float((end_point[1] - start_point[1]) / (end_point[0] - start_point[0]))
+            new_x = (((new_y - start_point[1]) / slope) + start_point[0])
+
+            slope2 = float((end_point2[1] - start_point2[1]) / (end_point2[0] - start_point2[0]))
+            new_x2 = (((new_y - start_point2[1]) / slope2) + start_point2[0])
+
+        else:
+
+            new_x = start_point[0]
+            new_x2 = start_point2[0]
+
+        coords1.append((int(new_x), new_y))
+
+        coords2.append((int(new_x2), new_y))
+
+        i += 1
+
+    return coords1, coords2
+
+
+def calc_line_points(start_point, end_point, start_point2, end_point2, image_height):
+    i = start_point[1]
+
+    coords1 = []
+    coords2 = []
+
+    while i < image_height:
+
+        new_y = i
+
+        # If we happen to have a perfectly vertical line, then we have no slope (prevents division by 0 error)
+        if (end_point[0] - start_point[0]) != 0:
+
+            slope = float((end_point[1] - start_point[1]) / (end_point[0] - start_point[0]))
+            new_x = (((new_y - start_point[1]) / slope) + start_point[0])
+
+            slope2 = float((end_point2[1] - start_point2[1]) / (end_point2[0] - start_point2[0]))
+            new_x2 = (((new_y - start_point2[1]) / slope2) + start_point2[0])
+
+        else:
+
+            new_x = start_point[0]
+            new_x2 = start_point2[0]
+
+        coords1.append((int(new_x), new_y))
+
+        coords2.append((int(new_x2), new_y))
+
+        i += 1
+
+    return coords1, coords2
--- src/experiments/geometric_scaling_python/perspective_calib.py
+++ src/experiments/geometric_scaling_python/perspective_calib.py
@@ -1,197 +1,145 @@
 from __future__ import division
+from point import Point
 
-__author__ = 'connorgoddard'
-
-import math
 import cv2
 import argparse
 import matplotlib.pyplot as plt
-from point import Point
+import geometrymath
 
-line_coords = []
-point_count = 0
-slope = 0
-lock_gui = False
-new_img = 0
-original_img = 0
+__author__ = 'connorgoddard'
 
-def calc_point(start_point, end_point, start_point2, end_point2):
-    global slope
 
-    height, width, depth = new_img.shape
+class PerspectiveCalibration:
+    def __init__(self, image_path):
+        self._point_coords = []
+        self._point_count = 0
+        self._line_slope = 0
+        self._lock_gui = False
 
-    i = start_point[1]
+        self._original_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        self._new_img = self._original_img.copy()
 
-    while i < height:
+        self.setup_image_gui(self._new_img)
 
-        new_y = i
 
-        # If we happen to have a perfectly vertical line, then we have no slope (prevents division by 0 error)
-        if (end_point[0] - start_point[0]) != 0:
+    def calc_point(self, start_point, end_point, start_point2, end_point2):
 
-            slope = float((end_point[1] - start_point[1]) / (end_point[0] - start_point[0]))
-            new_x = (((new_y - start_point[1]) / slope) + start_point[0])
+        height, width, depth = self._new_img.shape
 
-            slope2 = float((end_point2[1] - start_point2[1]) / (end_point2[0] - start_point2[0]))
-            new_x2 = (((new_y - start_point2[1]) / slope2) + start_point2[0])
+        # Here we are unwrapping the TUPLE returned from the function into two separate variables (coords1, coords2)
+        coords1, coords2 = geometrymath.calc_line_points(start_point, end_point, start_point2, end_point2, height)
 
-        else:
+        for i in range(len(coords1)):
+            cv2.circle(self._new_img, coords1[i], 2, (0, 0, 255), -1)
 
-            new_x = start_point[0]
-            new_x2 = start_point2[0]
+            cv2.circle(self._new_img, coords2[i], 2, (0, 0, 255), -1)
 
-        cv2.circle(new_img, (int(new_x), new_y), 2, (0, 0, 255), -1)
+            cv2.line(self._new_img, coords1[i], coords2[i], (0, 255, 255), 1)
 
-        cv2.circle(new_img, (int(new_x2), new_y), 2, (0, 0, 255), -1)
 
-        cv2.line(new_img, (int(new_x), new_y), (int(new_x2), new_y), (255, 255, 0), 1)
+    def render_line_reflection(self, start_point, end_point):
 
-        i += 1
+        height, width, depth = self._new_img.shape
 
+        # Here we are unwrapping the TUPLE returned from the function into two separate variables (coords1, coords2)
+        coords1, coords2 = geometrymath.calc_line_points_reflection(start_point, end_point, height, width)
 
-def calc_point_reflection(start_point, end_point):
-    global slope
+        for i in range(len(coords1)):
+            cv2.circle(self._new_img, coords1[i], 2, (0, 0, 255), -1)
 
-    height, width, depth = new_img.shape
+            cv2.circle(self._new_img, coords2[i], 2, (0, 0, 255), -1)
 
-    half_width = width / 2
+            cv2.line(self._new_img, coords1[i], coords2[i], (0, 255, 255), 1)
 
-    start_point2 = (half_width + (half_width - start_point[0]), start_point[1])
-    end_point2 = (half_width + (half_width - end_point[0]), end_point[1])
 
-    i = start_point[1]
+    def add_new_point(self, mouse_x, mouse_y):
+        new_point = Point(mouse_x, mouse_y)
+        self._point_coords.append(new_point.get_value())
 
-    while i < height:
+    def on_mouse_click(self, event):
+        toolbar = plt.get_current_fig_manager().toolbar
 
-        new_y = i
+        if toolbar.mode == '' and (event.xdata != None and event.ydata != None):
 
-        # If we happen to have a perfectly vertical line, then we have no slope (prevents division by 0 error)
-        if (end_point[0] - start_point[0]) != 0:
+            x = int(event.xdata)
+            y = int(event.ydata)
 
-            slope = float((end_point[1] - start_point[1]) / (end_point[0] - start_point[0]))
-            new_x = (((new_y - start_point[1]) / slope) + start_point[0])
+            if self._point_count < 4 and (self._lock_gui is False):
 
-            slope2 = float((end_point2[1] - start_point2[1]) / (end_point2[0] - start_point2[0]))
-            new_x2 = (((new_y - start_point2[1]) / slope2) + start_point2[0])
+                self.add_new_point(x, y)
+                cv2.circle(self._new_img, (x, y), 2, (255, 0, 0), -1)
 
-        else:
+                # If we have an even number of points, then we can draw a line between the last two points added to the collection.
+                if len(self._point_coords) % 2 == 0:
+                    cv2.line(self._new_img,
+                             (self._point_coords[len(self._point_coords) - 1][0],
+                              self._point_coords[len(self._point_coords) - 1][1]),
+                             (self._point_coords[len(self._point_coords) - 2][0],
+                              self._point_coords[len(self._point_coords) - 2][1]), (0, 255, 0),
+                             1)
 
-            new_x = start_point[0]
-            new_x2 = start_point2[0]
+                self.update_image_gui(self._new_img)
+                self._point_count += 1
 
-        cv2.circle(new_img, (int(new_x), new_y), 2, (0, 0, 255), -1)
 
-        cv2.circle(new_img, (int(new_x2), new_y), 2, (0, 0, 255), -1)
+    def on_key_press(self, event):
 
-        cv2.line(new_img, (int(new_x), new_y), (int(new_x2), new_y), (0, 255, 255), 1)
+        if event.key == 'r':
+            self.reset_image_gui()
 
-        i += 1
+        elif event.key == 'c':
 
+            if self._lock_gui is False and self._point_count == 4:
+                self.calc_point(self._point_coords[0], self._point_coords[1], self._point_coords[2],
+                                self._point_coords[3])
+                self.update_image_gui(self._new_img)
+                self._lock_gui = True
 
-def calc_vec_magnitude(point_1, point_2):
-    vx = (point_2[0] - point_1[0])
-    vy = (point_2[1] - point_1[1])
+        elif event.key == 'v':
 
-    return math.sqrt((vx * vx) + (vy * vy))
+            if self._lock_gui is False and self._point_count == 2:
+                self.render_line_reflection(self._point_coords[0], self._point_coords[1])
+                self.update_image_gui(self._new_img)
+                self._lock_gui = True
 
+        elif event.key == 'q':
+            plt.close()
 
-def add_new_point(mouse_x, mouse_y):
-    new_point = Point(mouse_x, mouse_y)
 
-    line_coords.append(new_point.value())
+    def setup_image_gui(self, image):
+        plt.axis("off")
 
+        canvas = plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-def on_mouse_click(event):
-    global new_img, point_count
+        canvas.figure.canvas.mpl_connect('button_press_event', self.on_mouse_click)
+        canvas.figure.canvas.mpl_connect('key_press_event', self.on_key_press)
 
-    toolbar = plt.get_current_fig_manager().toolbar
+        plt.show()
 
-    print toolbar.mode
 
-    if toolbar.mode == '' and (event.xdata != None and event.ydata != None):
+    def update_image_gui(self, image):
+        plt.clf()
+        plt.axis("off")
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.draw()
 
-        x = int(event.xdata)
-        y = int(event.ydata)
 
-        if point_count < 4 and (lock_gui is False):
+    def reset_image_gui(self):
 
-            add_new_point(x, y)
-            cv2.circle(new_img, (x, y), 2, (255, 0, 0), -1)
+        self._new_img = self._original_img.copy()
+        self.update_image_gui(self._new_img)
 
-            # If we have an even number of points, then we can draw a line between the last two points added to the collection.
-            if len(line_coords) % 2 == 0:
-                cv2.line(new_img, (line_coords[len(line_coords) - 1][0], line_coords[len(line_coords) - 1][1]),
-                         (line_coords[len(line_coords) - 2][0], line_coords[len(line_coords) - 2][1]), (0, 255, 0), 1)
-
-            update_image_gui(new_img)
-            point_count += 1
-
-
-def on_key_press(event):
-    global lock_gui
-
-    if event.key == 'r':
-        reset_image_gui()
-
-    elif event.key == 'c':
-
-        if lock_gui is False and point_count == 4:
-            calc_point(line_coords[0], line_coords[1], line_coords[2], line_coords[3])
-            update_image_gui(new_img)
-            lock_gui = True
-
-    elif event.key == 'v':
-
-        if lock_gui is False and point_count == 2:
-            calc_point_reflection(line_coords[0], line_coords[1])
-            update_image_gui(new_img)
-            lock_gui = True
-
-    elif event.key == 'q':
-        plt.close()
-
-
-def setup_image_gui(image):
-    plt.axis("off")
-
-    canvas = plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    canvas.figure.canvas.mpl_connect('button_press_event', on_mouse_click)
-    canvas.figure.canvas.mpl_connect('key_press_event', on_key_press)
-
-    plt.show()
-
-
-def update_image_gui(image):
-    plt.clf()
-    plt.axis("off")
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.draw()
-
-
-def reset_image_gui():
-    global new_img, point_count, lock_gui
-
-    new_img = original_img.copy()
-    update_image_gui(new_img)
-
-    point_count = 0
-    line_coords[:] = []
-    lock_gui = False
+        self._point_count = 0
+        self._point_coords[:] = []
+        self._lock_gui = False
 
 
 def main():
-    global original_img, new_img
-
     parser = argparse.ArgumentParser()
     parser.add_argument("inputImage", help="the first image")
     args = parser.parse_args()
 
-    original_img = cv2.imread(args.inputImage, cv2.IMREAD_COLOR)
-
-    new_img = original_img.copy()
-
-    setup_image_gui(new_img)
+    PerspectiveCalibration(args.inputImage)
 
 
 if __name__ == '__main__':  # if the function is the main function ...
