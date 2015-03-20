faa9c23e20cce5d1150539f4087f4b1ac7f615de
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-20 11:38:32 +0000

    Added support to create directory if non-exists before writing file. Fixes #65.

--- src/experiments/perspective_calibration/fileio.py
+++ src/experiments/perspective_calibration/fileio.py
@@ -1,22 +1,34 @@
 __author__ = 'connorgoddard'
-
+import os
 
 class FileIO:
     def __init__(self, filepath = "./"):
-        self._filepath = filepath
+        self._file_path = filepath
 
     def write_file(self, filename, data_prefix, data_collection, append):
 
+        self.check_directory(self._file_path)
+
         file_io_op = 'a' if append else 'w'
 
-        new_file = open(self._filepath + filename, file_io_op)
+        new_file = open(self._file_path + filename, file_io_op)
 
         new_file.write(data_prefix + "\n")
 
-        for point in data_collection:
-            new_file.write("{0},{1}\n".format(point[0], point[1]))
+        for data in data_collection:
+            new_file.write("{0},{1}\n".format(data[0], data[1]))
 
         new_file.close()
+
+    @staticmethod
+    def check_directory(directory_path):
+        try:
+            os.makedirs(directory_path)
+        except OSError:
+            if not os.path.isdir(directory_path):
+                raise
+
+
 
 
 
