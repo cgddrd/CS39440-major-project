20d19dfd404dadc4498944d7a9992a00bae07bb0
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-22 10:57:09 +0000

    Minor changes to setup.py.

--- src/experiments/perspective_calibration/setup.py
+++ src/experiments/perspective_calibration/setup.py
@@ -1,17 +1,23 @@
+from pip.req import parse_requirements
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements("./requirements.txt")
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 setup(
     name='perspective_calibration',
     version='1.0.0',
     packages=['', 'tests'],
-    install_requires=[
-        'matplotlib'
-    ],
+    install_requires=reqs,
     url='https://github.com/cgddrd/CS39440-major-project',
-    license='',
     author='Connor Goddard',
     author_email='connorlukegoddard@gmail.com',
     description=''
