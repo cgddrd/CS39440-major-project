bef7e24393fa1cee37a67e293b71a81b72003685
Connor Goddard <connorlukegoddard@gmail.com>
2015-03-22 13:31:21 +0000

    Minor change to setup.py and requirements.txt to upgrade pip.

--- src/experiments/perspective_calibration/setup.py
+++ src/experiments/perspective_calibration/setup.py
@@ -1,4 +1,5 @@
 from pip.req import parse_requirements
+from pip.download import PipSession
 
 try:
     from setuptools import setup
@@ -6,15 +7,14 @@ except ImportError:
     from distutils.core import setup
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("./requirements.txt")
+install_reqs = parse_requirements("./requirements.txt", session=PipSession())
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+# reqs is a list of requirements
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='perspective_calibration',
-    version='1.0.0',
+    version='1.0.1',
     packages=['', 'tests'],
     install_requires=reqs,
     url='https://github.com/cgddrd/CS39440-major-project',
