fc00f2f806547f2956d17948d753cdad80340687
Connor Goddard <clg11@aber.ac.uk>
2015-04-01 14:51:02 +0100

    Update setup.py

--- src/template_matching_scaling/setup.py
+++ src/template_matching_scaling/setup.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-from distutils.extension import Extension
+# from distutils.extension import Extension
 
 # Read from 'requirements.txt' file. Modified from: http://stackoverflow.com/a/16624700
 
@@ -21,13 +21,13 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 # setuptools DWIM monkey-patch madness
 # http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-if 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
+# if 'setuptools.extension' in sys.modules:
+#     m = sys.modules['setuptools.extension']
+#     m.Extension.__dict__ = m._Extension.__dict__
 
 # To install module in package folder, need to use package '.' notation in extension name.
 # See: https://github.com/uci-cbcl/tree-hmm/issues/2
-ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[numpy.get_include()])
+# ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[numpy.get_include()])
 
 setup(
     name='template_matching_scaling',
@@ -38,7 +38,7 @@ setup(
     author='Connor Goddard',
     author_email='connorlukegoddard@gmail.com',
     description='',
-    setup_requires=['cython', 'setuptools_cython', 'numpy'],
-    ext_modules=[ext]
+    setup_requires=['cython', 'setuptools_cython', 'numpy']
+    # ext_modules=[ext]
 )
 
