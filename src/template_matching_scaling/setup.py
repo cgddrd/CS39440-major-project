823b1d5109dbfed8396964e427a93f1177c3d54c
Connor Goddard <clg11@aber.ac.uk>
2015-04-01 18:27:36 +0100

    Update setup.py

--- src/template_matching_scaling/setup.py
+++ src/template_matching_scaling/setup.py
@@ -22,19 +22,19 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 # setuptools DWIM monkey-patch madness
 # http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-if 'setuptools.extension' in sys.modules:
-    m = sys.modules['setuptools.extension']
-    m.Extension.__dict__ = m._Extension.__dict__
+# if 'setuptools.extension' in sys.modules:
+#     m = sys.modules['setuptools.extension']
+#     m.Extension.__dict__ = m._Extension.__dict__
 
 # To install module in package folder, need to use package '.' notation in extension name.
 # See: https://github.com/uci-cbcl/tree-hmm/issues/2
 
-ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[np.get_include()])
+# ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[np.get_include()])
 
 setup(
     name='template_matching_scaling',
     version='2.1.0',
-    packages=['', 'tse_compiled', 'tests'],
+    packages=['tse', 'tse_compiled', 'tests'],
     # setup_requires=['cython', 'setuptools_cython'],
     install_requires=reqs,
     url='https://github.com/cgddrd/CS39440-major-project',
