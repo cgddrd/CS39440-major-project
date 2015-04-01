b241c06796f0b0fe172d6b00530b4f6bd35a3203
Connor Goddard <clg11@aber.ac.uk>
2015-04-01 16:38:23 +0100

    Update setup.py

--- src/template_matching_scaling/setup.py
+++ src/template_matching_scaling/setup.py
@@ -1,13 +1,14 @@
-# import numpy
-# import sys
+import numpy as np
+import sys
 
 from pip.req import parse_requirements
 from pip.download import PipSession
 
 try:
-    from setuptools import setup
+    from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup
+    from distutils.extension import Extension
 
 # from distutils.extension import Extension
 
@@ -21,24 +22,25 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 # setuptools DWIM monkey-patch madness
 # http://mail.python.org/pipermail/distutils-sig/2007-September/thread.html#8204
-# if 'setuptools.extension' in sys.modules:
-#     m = sys.modules['setuptools.extension']
-#     m.Extension.__dict__ = m._Extension.__dict__
+if 'setuptools.extension' in sys.modules:
+    m = sys.modules['setuptools.extension']
+    m.Extension.__dict__ = m._Extension.__dict__
 
 # To install module in package folder, need to use package '.' notation in extension name.
 # See: https://github.com/uci-cbcl/tree-hmm/issues/2
-# ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[numpy.get_include()])
+
+ext = Extension("tse_compiled.tse_c_imageutils", ["tse_compiled/tse_c_imageutils.pyx"], include_dirs=[np.get_include()])
 
 setup(
     name='template_matching_scaling',
     version='2.1.0',
     packages=['tse', 'tse_compiled', 'tests'],
+    setup_requires=['cython', 'setuptools_cython'],
     install_requires=reqs,
     url='https://github.com/cgddrd/CS39440-major-project',
     author='Connor Goddard',
     author_email='connorlukegoddard@gmail.com',
-    description=''
-    # setup_requires=['cython', 'setuptools_cython', 'numpy']
-    # ext_modules=[ext]
+    description='',
+    ext_modules=[ext]
 )
 
