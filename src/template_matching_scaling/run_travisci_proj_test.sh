9bc218110200f2b5a1d149833bfc320a0d7bbd38
Connor Goddard <clg11@aber.ac.uk>
2015-04-01 11:43:19 +0100

    Update run_travisci_proj_test.sh

--- src/template_matching_scaling/run_travisci_proj_test.sh
+++ src/template_matching_scaling/run_travisci_proj_test.sh
@@ -1,12 +1,12 @@
 
 # Compile/build the Cython 'C' extensions.
-# python setup.py build_ext --inplace
+python setup.py build_ext --inplace
 
 # Run the 'setup.py' script to install dependencies etc.
 python setup.py install
 
 # Actually install the Python module.
-pip install .
+# pip install .
 
 ls -l
 
