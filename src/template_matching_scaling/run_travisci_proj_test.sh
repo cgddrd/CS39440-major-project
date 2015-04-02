7cee5b913e506e6c30e3c7dd35f8ae283d5ddd0c
Connor Goddard <clg11@aber.ac.uk>
2015-04-02 07:20:13 +0100

    Update run_travisci_proj_test.sh

--- src/template_matching_scaling/run_travisci_proj_test.sh
+++ src/template_matching_scaling/run_travisci_proj_test.sh
@@ -2,15 +2,15 @@
 pip install -r requirements.txt
 
 # Compile/build the Cython 'C' extensions.
-python setup.py build_ext --inplace
+# python setup.py build_ext --inplace
+
+# Run the 'setup.py' script to install dependencies etc.
+python setup.py install
 
 # Just checking the 'Cython' files have been compiled.
 cd tse_compiled
 ls -l
 cd ..
-
-# Run the 'setup.py' script to install dependencies etc.
-python setup.py install
 
 # Run the automated unti tests with coverage.
 nosetests -w tests/ --with-coverage
